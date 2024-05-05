e69f08aa711370b78bde89548636ea60a5675ddb
Gihanmadhusankha <gmjayaushantha2007@gmail.com>
2024-05-05 21:05:13 +0530

    f9

--- login-sign/src/Components/LoginSignup/LoginSignup.jsx
+++ login-sign/src/Components/LoginSignup/LoginSignup.jsx
@@ -31,7 +31,7 @@ const LoginSignup = () => {
                 <input type="password"placeholder='Password'/>
             </div>
          </div>
-         {action==="Sign Up"?<div></div>:<div className="forget-passsword">Lost Password ? <span>Click Here!</span></div>}
+         {action==="Sign Up"?<div></div>:<div className="forgot-passsword">Lost Password ? <span>Click Here!</span></div>}
           
          <div className="submit-container">
             <div className={action === "Login"?"submit gray":"submit"}onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
